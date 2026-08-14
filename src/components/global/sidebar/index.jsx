8aae95f75b758e4749c8b50f0ee4f40455329898
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2026-08-14 14:32:02 +0530

    update : code fix

--- src/components/global/sidebar/index.jsx
+++ src/components/global/sidebar/index.jsx
@@ -12,16 +12,6 @@ export function AppSidebar({ brand, ...props }) {
         <Sidebar {...props}>
             <SidebarHeader className="relative">
                 <SidebarBrand brand={brand} />
-                {isMobile && (
-                    <button
-                        type="button"
-                        onClick={() => setOpenMobile(false)}
-                        className="absolute right-3.5 top-1/2 -translate-y-1/2 z-50 flex size-8 items-center justify-center rounded-lg border border-gray-200 bg-white text-gray-500 shadow-xs transition-all hover:bg-gray-50 hover:text-gray-900 active:scale-95 dark:border-zinc-800 dark:bg-zinc-900 dark:text-zinc-400 dark:hover:bg-zinc-800 dark:hover:text-white"
-                        aria-label="Close sidebar"
-                    >
-                        <X className="size-4" />
-                    </button>
-                )}
             </SidebarHeader>
             <SidebarContent>
                 <SidebarNav />
